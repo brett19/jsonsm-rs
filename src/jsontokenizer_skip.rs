3f8ccbd9ff280fdc404a6682856a1e4446c51647
Brett Lawson <brett19@gmail.com>
2024-11-12 21:50:18 -0800

    use iterators

--- src/jsontokenizer_skip.rs
+++ src/jsontokenizer_skip.rs
@@ -5,40 +5,38 @@ type TokenizerFnResult = std::result::Result<usize, (TokenizerErrorType, usize)>
 #[inline]
 fn skip_string(data: &[u8], pos: usize) -> TokenizerFnResult {
     let mut is_escaped = false;
-    for pos in pos + 1..data.len() {
+    match data[pos + 1..].iter().position(|&c| {
         if is_escaped {
             is_escaped = false;
+            false
         } else {
-            match data[pos] {
-                b'"' => {
-                    return Ok(pos + 1);
-                }
+            match c {
+                b'"' => true,
                 b'\\' => {
                     is_escaped = true;
+                    false
                 }
-                _ => {}
+                _ => false,
             }
         }
+    }) {
+        Some(end_pos) => Ok(pos + 1 + end_pos + 1),
+        None => Err((TokenizerErrorType::UnexpectedEndOfInput, data.len())),
     }
-
-    return Err((TokenizerErrorType::UnexpectedEndOfInput, data.len()));
 }
 
 #[inline]
 fn skip_number(data: &[u8], pos: usize) -> TokenizerFnResult {
-    for pos in pos + 1..data.len() {
-        match data[pos] {
-            b'0'..=b'9' => {}
-            b'.' => {}
-            b'e' | b'E' => {}
-            b'+' | b'-' => {}
-            _ => {
-                return Ok(pos);
-            }
-        }
+    match data[pos + 1..].iter().position(|&c| match c {
+        b'0'..=b'9' => false,
+        b'.' => false,
+        b'e' | b'E' => false,
+        b'+' | b'-' => false,
+        _ => true,
+    }) {
+        Some(end_pos) => Ok(pos + 1 + end_pos),
+        None => Ok(data.len()),
     }
-
-    Ok(data.len())
 }
 
 #[inline]
@@ -158,13 +156,16 @@ fn skip_out_of_object_or_array(data: &[u8], pos: usize) -> TokenizerFnResult {
     let mut depth = 1;
     let mut pos = pos;
     while pos < data.len() {
+        pos = match data[pos..].iter().position(|&c| match c {
+            b' ' | b'\t' | b'\n' | b'\r' => false,
+            b':' | b',' => false,
+            _ => true,
+        }) {
+            Some(end_pos) => pos + end_pos,
+            None => return Err((TokenizerErrorType::UnexpectedEndOfInput, data.len())),
+        };
+
         match data[pos] {
-            b' ' | b'\t' | b'\n' | b'\r' => {
-                pos += 1;
-            }
-            b':' | b',' => {
-                pos += 1;
-            }
             b'{' | b'[' => {
                 depth += 1;
                 pos += 1;
@@ -191,18 +192,13 @@ fn skip_out_of_object_or_array(data: &[u8], pos: usize) -> TokenizerFnResult {
 
 #[inline]
 fn skip_over_value(data: &[u8], pos: usize) -> TokenizerFnResult {
-    let mut pos = pos;
-    while pos < data.len() {
-        match data[pos] {
-            b' ' | b'\t' | b'\n' | b'\r' => {}
-            _ => break,
-        }
-        pos += 1;
-    }
-
-    if pos >= data.len() {
-        return Err((TokenizerErrorType::UnexpectedEndOfInput, data.len()));
-    }
+    let pos = match data[pos..].iter().position(|&c| match c {
+        b' ' | b'\t' | b'\n' | b'\r' => false,
+        _ => true,
+    }) {
+        Some(pos) => pos,
+        None => return Err((TokenizerErrorType::UnexpectedEndOfInput, data.len())),
+    };
 
     match data[pos] {
         b'{' | b'[' => return skip_out_of_object_or_array(data, pos + 1),
